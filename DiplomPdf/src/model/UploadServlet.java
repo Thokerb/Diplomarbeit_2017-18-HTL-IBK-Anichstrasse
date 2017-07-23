aa478bd97b1724606c9025f056b889e57b3c3205
Thomas Kerber <kerber.tom98@gmail.com>
2017-07-23 17:05:39 +0200

    blub

--- DiplomPdf/src/model/UploadServlet.java
+++ DiplomPdf/src/model/UploadServlet.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -13,6 +14,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 
 import com.google.gson.Gson;
 
@@ -38,8 +42,7 @@ public class UploadServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		String answer = request.getParameter("answer");
-		System.out.println(answer);
+
 		
 		
 		
@@ -52,9 +55,21 @@ public class UploadServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
-        InputStream fileContent = filePart.getInputStream();
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
+	    Part filePart = request.getPart("pdffile"); // Retrieves <input type="file" name="file">
+        InputStream fileContent = filePart.getInputStream(); 
+        String dateiname = request.getParameter("dateiname");
+        System.out.println(dateiname);
+
+        
+        
+        int size = request.getContentLength();
+        byte[] bytes = new byte[1024];
+        
+        File uploads = new File("C:/Temp");
+        
+        File file = new File(uploads, dateiname);
+
+        Files.copy(fileContent, file.toPath());
         
 
         
