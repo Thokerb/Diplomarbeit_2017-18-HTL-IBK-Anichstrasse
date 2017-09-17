22bfd505b17db52bb2bd2b21c2f6f2f157f2557d
Thomas Kerber <kerber.tom98@gmail.com>
2017-09-17 18:59:07 +0200

    downloadButton mit Servlet fertig eingebunden

--- DiplomPdf/model/DownloadServlet.java
+++ DiplomPdf/model/DownloadServlet.java
@@ -27,7 +27,8 @@ public class DownloadServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Ich wurde aufgerufen");
+		String antwort = request.getParameter("download");
+		System.out.println(antwort);
 		
 	}
 
