44c9f714b572ee122aef7218cfc4b416dcc9b1d6
HindelangS <Sara.Hindelang@students.htlinn.ac.at>
2017-10-19 18:01:38 +0200

    Lesen von Textfiles (.txt)

---
+++ DiplomPdf/model/TextdateiLesen.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+
+import com.sun.javafx.util.Utils; 
+
+public class TextdateiLesen {
+
+	public static void main(String[] args) {
+		// Hier Datei angeben, wessen Text gelesen werden sollte 
+		String fileName = "C://Users//Sara//Dropbox//Diplomarbeit//Text.txt";
+
+		// Zwischenspeicher der Zeilen 
+		String line = null;
+
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}   
+			bufferedReader.close();         
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("Datei  '" + fileName + "' kann nicht geöffnet werden ");                
+		}
+		catch(IOException ex) {
+			System.out.println("Datei '" + fileName + "' konnte nicht gelesen werden");                  
+			// ex.printStackTrace();
+		}
+
+	}
+}
