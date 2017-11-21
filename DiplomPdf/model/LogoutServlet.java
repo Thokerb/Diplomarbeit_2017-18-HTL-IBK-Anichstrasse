f219d470b351fed49c51044b14da167b4c0612fb
Thomas Kerber <kerber.tom98@gmail.com>
2017-11-21 21:56:10 +0100

    web.xml update

--- DiplomPdf/model/LogoutServlet.java
+++ DiplomPdf/model/LogoutServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/LogoutServlet")
+//@WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
