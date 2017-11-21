7c1d7352f5eb21466f371b8ef3de8e86108a4d67
Thomas Kerber <kerber.tom98@gmail.com>
2017-11-21 22:11:39 +0100

    sm fix2

--- DiplomPdf/model/LoginServlet.java
+++ DiplomPdf/model/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/LoginServlet")
+//@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
