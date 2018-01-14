1db8f7c85d1a5506c16d0357c7b77c3ba3a299dc
HindelangS <Sara.Hindelang@students.htlinn.ac.at>
2018-01-14 13:22:54 +0100

    Conflict?

--- DiplomPdf/model/DataTableServlet.java
+++ DiplomPdf/model/DataTableServlet.java
@@ -112,10 +112,6 @@ public class DataTableServlet extends HttpServlet {
 			conn=db.getConnection();
 
 			//list = db.readDaten(conn);
-			if(search!=null&&!search.isEmpty())
-			{
-				sortierparameter="suchwort";
-			}
 			
 			switch(sortierparameter){
 
@@ -190,7 +186,7 @@ public class DataTableServlet extends HttpServlet {
 				}
 				break; 
 			}
-
+/*
 			case "suchwort" :{
 				daten=db.ranking2(search);
 				for(int i=0;i<daten.size();i++)
@@ -198,7 +194,7 @@ public class DataTableServlet extends HttpServlet {
 					System.out.println(daten.get(i)[1]);
 				}
 				break;
-			}
+			}*/
 			
 			default:{
 
@@ -207,9 +203,10 @@ public class DataTableServlet extends HttpServlet {
 				{
 					System.out.println(daten.get(i)[1]);
 				}
-				System.out.println("Die Daten wurden nach dem Dateinamen alphabetisch geordnet");
+				System.out.println("Die Daten wurden nach Autor alphabetisch geordnet");
 			}
 			}
+			
 			anzahl=db.AnzahlEinträge(conn);
 
 
