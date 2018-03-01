24fa525ade5b4a864fa31c5844b3a97fc35ae5cb
Verena <verena.gu@aon.at>
2018-03-01 19:31:08 +0100

    gelöschte Daten in externe Tabelle finish

--- DiplomPdf/model/DeleteServlet.java
+++ DiplomPdf/model/DeleteServlet.java
@@ -3,7 +3,6 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,60 +23,64 @@ import model.Daten;
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-  
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		//	response.getWriter().append("Served at: ").append(request.getContextPath());
 		/**
 		 * Hier werden die Daten der Datei geschickt, welche gelöscht werden sollen
 		 * Für ein Beispiel testjquery.html öffnen und auf den delete button klicken4
 		 * TODO: aus DB löschen aber darauf achten, dass nicht zu schnell gelöscht wird.
 		 * evtl reihenfolge vorgeben zuerst db dann aus seite löschen? 
 		 */
-		String toshift = request.getParameter("todelete");
-		System.out.println("toshift: "+toshift);
-		
+		String todelete = request.getParameter("todelete");
+		System.out.println("todelete: "+ todelete);
+
 		Gson gsn = new Gson();
 		JsonObject jobj; 
 
-		jobj = gsn.fromJson(toshift, JsonObject.class);
+		jobj = gsn.fromJson(todelete, JsonObject.class);
 		String idObj = jobj.get("ID").getAsString();
 		int id = Integer.parseInt(idObj);
-		System.out.println("toshift:"+id);
-		String autor = jobj.get("Autor").getAsString();
+		System.out.println("todelete:" +id);
 
-	//	String autor = jobj.get("Autor").getAsString();
+		//	String autor = jobj.get("Autor").getAsString();
 		HttpSession ses = request.getSession(false);
 		String username = (String) ses.getAttribute("user"); //Username wird vom vorherigen Servlet genommen
-		
+
 		Daten uploaddaten = new Daten();
 
 		DBManager db;
 		String uploader = null;
+		try {
+			db = new DBManager();
+			Connection con = db.getConnection();
+			uploader = db.getDateiinfo(id, con,"uploaddaten","uploadid");
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-		System.out.println(username+"|"+uploader);
-		
+		System.out.println(username +"|"+ uploader);
+
 		if(username.equals(uploader)){
 			try {
-				System.out.println("PENIS PENIS PENis");
-				
+				System.out.println("PENIS PENIS PENIS");
+
 				DBManager db1 = new DBManager();
 				Connection conn=db1.getConnection();
 				uploaddaten=db1.readzuloeschendeDatei(conn,id);
-				db1.Datenlöschen(conn,id,"uploaddaten");
 				db1.writegeloeschteDaten(conn, uploaddaten);
+				db1.Datenlöschen(conn,id,"uploaddaten","uploadid");
 				System.out.println("PENIS PENIS PENSI");
-				
-				db1.Datenlöschen(conn,id,"uploaddaten");
 
 			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -91,7 +94,6 @@ public class DeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 }
